fc9609f5654cd172069de92eb97f48f60f7545e5
sariyaesc <escamillasara49@gmail.com>
2023-08-03 21:48:30 -0600

    commit

--- api/main.py
+++ api/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 app= FastAPI()
 
+
 @app.get("/")
 def raiz():
-    return{"Hello": "World"}
+    return{"Hello": "Hola"}
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}/{m}")
 def read_items(item_id:int,m:str=None):
     return {"item_id": item_id,"m":m}
