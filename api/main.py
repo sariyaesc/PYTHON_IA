18f8b8eb8724cf8acc11c59ad05774a027f52df9
sariyaesc <escamillasara49@gmail.com>
2023-08-06 11:15:43 -0600

    commit

--- api/main.py
+++ api/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app= FastAPI()
+
+class Libro(BaseModel):
+    titulo:str
+    autor:str
+    paginas:int
+    editorial:Optional[str]
 
 
 @app.get("/")
 def raiz():
-    return{"Hello": "Hola"}
+    return{"Message": "holaa <3"}
 
-@app.get("/items/{item_id}/{m}")
-def read_items(item_id:int,m:str=None):
-    return {"item_id": item_id,"m":m}
+@app.get("/libros/{id}")
+async def mostrar_libro(id:int):
+    return {"data":id}
+
+@app.post("/libros/")
+async def insertar_libro(libro:Libro):
+    return {"message":f"Libro:{libro.titulo} insertado correctamente"}
+
